a6b97243d25341828ca306a43f0dd5d04a1ac339
XuZhenYu289 <490365345@qq.com>
2017-10-19 01:35:30 +0800

    Add files via upload

---
+++ HASH.R
@@ -0,0 +1,7 @@
+
+read.table("clipboard",header=T)
+
+digest('I learn a lot from this class when I am proper listening to the professor','sha256')
+#[1] "c16700de5a5c1961e279135f2be7dcf9c187cb6b21ac8032308c715e1ce9964c"
+digest('I do not learn a lot from this class when I am absent and playing on my Iphone','sha256')
+#[1] "2533d529768409d1c09d50451d9125fdbaa6e5fd4efdeb45c04e3c68bcb3a63e"
